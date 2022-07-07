c44fa178ed14ad562ee03d416ebd4f869f84af37
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-07-07 16:52:51 +0900

    create basic directories and setup nodejs project

---
+++ datamanager/nodejs/scheme/validator.test.ts
@@ -0,0 +1,13 @@
+import SchemeValidater from './validator'
+
+describe('scheme validator', () => {
+	const schemeValidater = new SchemeValidater()
+
+	test('find missing items for public facilities', () => {
+		const missingItems = schemeValidater.missingItems({
+			current_items: ['方書', '都道府県コード又は市区町村コード', '名称'],
+			category: 'public-facilities'
+		})
+		expect(missingItems.length).toBe(21)
+	})
+})
