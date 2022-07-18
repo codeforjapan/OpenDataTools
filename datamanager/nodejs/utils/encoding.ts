51fec8e80c83c684ccddaa5058340687bbae2aca
kota-yata <kota@yatagai.com>
2022-07-19 00:21:08 +0900

    Write encoding test

--- datamanager/nodejs/utils/encoding.ts
+++ datamanager/nodejs/utils/encoding.ts
@@ -1,14 +1,27 @@
 import Encoding from 'encoding-japanese';
 
-export const EncodingConverter = (
-  data: ArrayBufferLike, to: Encoding.Encoding
-): string => {
-  const code = new Uint8Array(data);
-  const originalEncoding = Encoding.detect(code) as Encoding.Encoding;
-  const utf8Str: string = Encoding.convert(code, {
-    to,
-    from: originalEncoding,
-    type: 'string'
-  });
-  return utf8Str;
+export default class EncodingConverter {
+  private static convert(data: Buffer, to: Encoding.Encoding): number[] {
+    const code = new Uint8Array(data);
+    const originalEncoding = Encoding.detect(code) as Encoding.Encoding;
+    const convertedArray: number[] = Encoding.convert(code, {
+      to,
+      from: originalEncoding,
+    });
+    return convertedArray;
+  }
+  public static asArray(data: Buffer, to: Encoding.Encoding): number[] {
+    const array = EncodingConverter.convert(data, to);
+    return array;
+  }
+  public static asString(data: Buffer, to: Encoding.Encoding): string {
+    const array = EncodingConverter.convert(data, to);
+    const string = Encoding.codeToString(array);
+    return string;
+  }
+  public static asBuffer(data: Buffer, to: Encoding.Encoding): Buffer {
+    const array = EncodingConverter.convert(data, to);
+    const buffer = Buffer.from(array);
+    return buffer;
+  }
 };
