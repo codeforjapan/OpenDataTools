eec997662e165c79d4fb64254bb17542fee8cfcb
kota-yata <kota@yatagai.com>
2022-09-21 20:09:52 +0900

    Add Region Code Validator

---
+++ datamanager/nodejs/items/validator/regcode.test.ts
@@ -0,0 +1,25 @@
+import { ItemValidatorRegcode } from './regcode';
+
+describe('items/validator/regcode', () => {
+  const regcodeValidator = new ItemValidatorRegcode();
+
+  test('文字列かどうか', () => {
+    expect(() => regcodeValidator.validateDataType(1000)).toThrow('都道府県・市区町村コードは文字列である必要があります。');
+    expect(() => regcodeValidator.validateDataType(true)).toThrow('都道府県・市区町村コードは文字列である必要があります。');
+  });
+
+  test('6桁かどうか', () => {
+    expect(() => regcodeValidator.validateDataType('adfdsff')).toThrow('都道府県・市区町村コードは6桁である必要があります。');
+    expect(() => regcodeValidator.validateDataType('')).toThrow('都道府県・市区町村コードは6桁である必要があります。');
+  });
+
+  test('数値に変換可能かどうか', () => {
+    expect(() => regcodeValidator.validateDataType('0xd345')).toThrow('都道府県・市区町村コードは数値である必要があります。');
+    expect(() => regcodeValidator.validateDataType('１２３４５６')).toThrow('都道府県・市区町村コードは数値である必要があります。');
+  });
+
+  test('範囲内の数値かどうか', () => {
+    expect(() => regcodeValidator.validateDataType('010000')).toThrow('都道府県・市区町村コードの数値が範囲を超えています。');
+    expect(() => regcodeValidator.validateDataType('509932')).toThrow('都道府県・市区町村コードの数値が範囲を超えています。');
+  });
+});
