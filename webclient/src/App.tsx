5af0f9d61064c29749d1862c0661c55c03f89ff5
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-09-25 16:40:33 +0900

    create pages

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -1,11 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { AppProvider } from './providers/app';
 import { AppRoutes } from './routes';
 
 function App() {
   return (
-    <AppProvider>
-      <AppRoutes />
-    </AppProvider>
+    <ChakraProvider>
+      <AppProvider>
+        <AppRoutes />
+      </AppProvider>
+    </ChakraProvider>
   );
 }
 
---
+++ webclient/src/features/map/routes/Map.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+import { FC } from 'react';
+import { ContentLayout } from '../../../components/Layout';
+
+export const Map: FC = () => {
+  return (
+    <ContentLayout title="マップ">
+      <Box></Box>
+    </ContentLayout>
+  );
+};
