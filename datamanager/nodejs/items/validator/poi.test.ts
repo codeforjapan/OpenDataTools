8f5361074381d6cf06992beb30fe99073c65a24e
Junya Ishihara <junya@champierre.com>
2022-10-01 19:47:48 +0900

    文字列であるかどうか、4桁であるかどうか、数値であるかどうかのチェックも追加。

--- datamanager/nodejs/items/validator/poi.test.ts
+++ datamanager/nodejs/items/validator/poi.test.ts
@@ -3,9 +3,22 @@ import { ItemValidatorPoi } from './poi';
 describe('items/validator/poi', () => {
   const poiValidator = new ItemValidatorPoi();
 
-  test('POIかどうか', () => {
-    expect(() => poiValidator.validateDataType('テスト')).toThrow('不正なPOIデータです。');
-    expect(() => poiValidator.validateDataType('1000')).toThrow('不正なPOIデータです。');
-    expect(() => poiValidator.validateDataType('0101')).not.toThrow('不正なPOIデータです。');
+  test('文字列かどうか', () => {
+    expect(() => poiValidator.validateDataType(1000)).toThrow('POIコードは文字列である必要があります。');
+    expect(() => poiValidator.validateDataType(true)).toThrow('POIコードは文字列である必要があります。');
+  });
+
+  test('4桁かどうか', () => {
+    expect(() => poiValidator.validateDataType('01010')).toThrow('POIコードは4桁である必要があります。');
+    expect(() => poiValidator.validateDataType('')).toThrow('POIコードは4桁である必要があります。');
+  });
+
+  test('数値かどうか', () => {
+    expect(() => poiValidator.validateDataType('abcd')).toThrow('POIコードは数値である必要があります。');
+  });
+
+  test('正しいPOIコードかどうか', () => {
+    expect(() => poiValidator.validateDataType('0101')).not.toThrow('正しくないPOIコードです。');
+    expect(() => poiValidator.validateDataType('1000')).toThrow('正しくないPOIコードです。');
   });
 });
--- datamanager/nodejs/items/validator/poi.ts
+++ datamanager/nodejs/items/validator/poi.ts
@@ -2,15 +2,28 @@ import fs from 'fs';
 import { parse } from 'csv-parse/sync';
 
 export class ItemValidatorPoi {
+  static INT_EXP = /[^0-9]/;
   validateDataType(data: any) {
+    if (typeof data != 'string') {
+      throw new Error('POIコードは文字列である必要があります。');
+    }
+
+    if (data.length !== 4) {
+      throw new Error('POIコードは4桁である必要があります。');
+    }
+
+    const matched = data.match(ItemValidatorPoi.INT_EXP);
+    if (matched !== null) {
+      throw new Error('POIコードは数値である必要があります。');
+    }
+
     const csvData = parse(fs.readFileSync(__dirname + '/data/poi.csv'));
     // 先頭行はヘッダーなので削除
     csvData.shift();
     const poiList = csvData.map((row: any) => row[1]);
     if (!poiList.includes(data)) {
-      throw new Error('不正なPOIデータです。');
+      throw new Error('正しくないPOIコードです。');
     }
-
     return;
   }
 }
