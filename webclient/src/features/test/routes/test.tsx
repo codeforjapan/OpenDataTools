a1e275a8668ddf00b43c7c5ed434fdd1a5c9cd54
Kazuki Imamura <kaizumaki@gmail.com>
2022-10-03 05:37:22 +0900

    コンポーネントOstInputを追加

--- webclient/src/features/test/routes/test.tsx
+++ webclient/src/features/test/routes/test.tsx
@@ -1,11 +1,13 @@
 import { Link } from '../../../components/Elements/Link';
+import { OstInput } from '../../../components/Elements/OstInput';
 import { OstTextarea } from '../../../components/Elements/OstTextarea';
 
 export const Test = () => {
   return (
     <>
       <Link />
-      <OstTextarea label="ラベル" />
+      <OstInput label="inputフィールド" />
+      <OstTextarea label="テキストエリア" />
     </>
   );
 };
