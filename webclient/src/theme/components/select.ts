4c4e87bc45940791d72ec62b8aa3c52c50adffb1
Kazuki Imamura <kaizumaki@gmail.com>
2022-10-03 07:08:31 +0900

    prettier整形

--- webclient/src/theme/components/select.ts
+++ webclient/src/theme/components/select.ts
@@ -1,47 +1,43 @@
-import { selectAnatomy as parts } from "@chakra-ui/anatomy"
-import {
-  createMultiStyleConfigHelpers,
-  defineStyle,
-} from "@chakra-ui/styled-system"
-import { mode } from "@chakra-ui/theme-tools"
-import { inputTheme } from "./input"
+import { selectAnatomy as parts } from '@chakra-ui/anatomy';
+import { createMultiStyleConfigHelpers, defineStyle } from '@chakra-ui/styled-system';
+import { mode } from '@chakra-ui/theme-tools';
+import { inputTheme } from './input';
 
-const { defineMultiStyleConfig, definePartsStyle } =
-  createMultiStyleConfigHelpers(parts.keys)
+const { defineMultiStyleConfig, definePartsStyle } = createMultiStyleConfigHelpers(parts.keys);
 
 const baseStyleField = defineStyle((props) => {
   return {
     ...inputTheme.baseStyle?.field,
-    bg: mode("white", "gray.700")(props),
-    appearance: "none",
-    paddingBottom: "1px",
-    lineHeight: "normal",
-    "> option, > optgroup": {
-      bg: mode("white", "gray.700")(props),
+    bg: mode('white', 'gray.700')(props),
+    appearance: 'none',
+    paddingBottom: '1px',
+    lineHeight: 'normal',
+    '> option, > optgroup': {
+      bg: mode('white', 'gray.700')(props),
     },
-  }
-})
+  };
+});
 
 const baseStyleIcon = defineStyle({
-  width: "6",
-  height: "100%",
-  insetEnd: "2",
-  position: "relative",
-  color: "currentColor",
-  fontSize: "xl",
+  width: '6',
+  height: '100%',
+  insetEnd: '2',
+  position: 'relative',
+  color: 'currentColor',
+  fontSize: 'xl',
   _disabled: {
     opacity: 0.5,
   },
-})
+});
 
 const baseStyle = definePartsStyle((props) => ({
   field: baseStyleField(props),
   icon: baseStyleIcon,
-}))
+}));
 
 const iconSpacing = defineStyle({
-  paddingInlineEnd: "8",
-})
+  paddingInlineEnd: '8',
+});
 
 const sizes = {
   lg: {
@@ -72,14 +68,14 @@ const sizes = {
       ...iconSpacing,
     },
     icon: {
-      insetEnd: "1",
+      insetEnd: '1',
     },
   },
-}
+};
 
 export const selectTheme = defineMultiStyleConfig({
   baseStyle,
   sizes,
   variants: inputTheme.variants,
   defaultProps: inputTheme.defaultProps,
-})
+});
