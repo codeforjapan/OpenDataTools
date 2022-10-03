ac40b4c7302242f205c74c388aaeeffbec0e4791
Kazuki Imamura <kaizumaki@gmail.com>
2022-10-03 13:55:02 +0900

    コンポーネントOstButtonを追加

--- webclient/src/features/test/routes/test.tsx
+++ webclient/src/features/test/routes/test.tsx
@@ -1,12 +1,16 @@
+import { Stack, Avatar } from '@chakra-ui/react';
+import { PhoneIcon, AddIcon, WarningIcon } from '@chakra-ui/icons';
+
 import { Link } from '../../../components/Elements/Link';
 import { OstInput } from '../../../components/Elements/OstInput';
 import { OstTextarea } from '../../../components/Elements/OstTextarea';
 import { OstSelect } from '../../../components/Elements/OstSelect';
 import { OstCheckbox } from '../../../components/Elements/OstCheckbox';
+import { OstButton } from '../../../components/Elements/OstButton';
 
 export const Test = () => {
   return (
-    <>
+    <div>
       <Link />
       <OstInput label="テキストフィールド" />
       <OstTextarea label="テキストエリア" />
@@ -20,6 +24,62 @@ export const Test = () => {
       />
       <OstCheckbox label="チェック１" />
       <OstCheckbox label="チェック２" isDisabled={true} />
-    </>
+      <OstButton label="ボタン" view="button" size="L" />
+      <OstButton label="ボタン" view="button" size="L" isDisabled={true} />
+      <OstButton label="ボタン" view="button" size="L" icon={<PhoneIcon />} />
+      <OstButton label="ボタン" view="button" size="L" isDisabled={true} icon={<PhoneIcon />} />
+      <OstButton
+        label="ボタン"
+        view="skeleton"
+        size="L"
+        icon={<Avatar bg="bg.active" size="md" p="12px" icon={<PhoneIcon />} />}
+      />
+      <OstButton
+        label="ボタン"
+        view="skeleton"
+        size="L"
+        isDisabled={true}
+        icon={<Avatar size="md" p="12px" icon={<PhoneIcon />} />}
+      />
+      <OstButton
+        view="icon-only"
+        size="L"
+        icon={<Avatar bg="bg.active" size="md" p="12px" icon={<PhoneIcon />} />}
+      />
+      <OstButton
+        view="icon-only"
+        size="L"
+        isDisabled={true}
+        icon={<Avatar size="md" p="12px" icon={<PhoneIcon />} />}
+      />
+      <OstButton label="ボタン" view="button" size="S" />
+      <OstButton label="ボタン" view="button" size="S" isDisabled={true} />
+      <OstButton label="ボタン" view="button" size="S" icon={<PhoneIcon />} />
+      <OstButton label="ボタン" view="button" size="S" isDisabled={true} icon={<PhoneIcon />} />
+      <OstButton
+        label="ボタン"
+        view="skeleton"
+        size="S"
+        icon={<Avatar bg="bg.active" size="sm" icon={<PhoneIcon />} />}
+      />
+      <OstButton
+        label="ボタン"
+        view="skeleton"
+        size="S"
+        isDisabled={true}
+        icon={<Avatar size="sm" icon={<PhoneIcon />} />}
+      />
+      <OstButton
+        view="icon-only"
+        size="S"
+        icon={<Avatar bg="bg.active" size="sm" icon={<PhoneIcon />} />}
+      />
+      <OstButton
+        view="icon-only"
+        size="S"
+        isDisabled={true}
+        icon={<Avatar size="sm" icon={<PhoneIcon />} />}
+      />
+    </div>
   );
 };
