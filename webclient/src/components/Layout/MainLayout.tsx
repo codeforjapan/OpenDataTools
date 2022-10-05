09ad3c23ddda498e46b6aae31329855b1542b04f
Kazuki Imamura <kaizumaki@gmail.com>
2022-10-05 23:08:48 +0900

    MainLayoutのスタイルを調整

--- webclient/src/components/Layout/MainLayout.tsx
+++ webclient/src/components/Layout/MainLayout.tsx
@@ -1,59 +1,18 @@
 /** @jsxImportSource @emotion/react */
 import * as React from 'react';
 import { css } from '@emotion/react';
-import { NavLink } from 'react-router-dom';
 
 const wrapperStyle = css`
   display: grid;
-  grid-template-rows: auto 1fr auto;
+  grid-template-rows: 1fr auto;
   grid-template-columns: 100%;
   min-height: 100vh;
+  background-color: var(--chakra-colors-body-bg);
 `;
 
-const commonElementStyle = css`
-  background-color: var(--chakra-colors-gray-light);
+const footerStyle = css`
   padding: 30px;
 `;
-
-const mainContainerStyle = css`
-  margin: 0 auto;
-  width: 90%;
-  max-width: 1200px;
-`;
-
-type NavigationItem = {
-  name: string;
-  to: string;
-  icon: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
-};
-
-const Navigation = () => {
-  const navigation = [
-    { name: 'File Upload', to: '.' },
-    { name: 'Select Category', to: './select-category' },
-  ].filter(Boolean) as NavigationItem[];
-
-  return (
-    <div
-      css={css`
-        display: flex;
-      `}
-    >
-      {navigation.map((item, index) => (
-        <div
-          key={item.name}
-          css={css`
-            margin: 0 8px;
-          `}
-        >
-          <NavLink end={index === 0} to={item.to}>
-            {item.name}
-          </NavLink>
-        </div>
-      ))}
-    </div>
-  );
-};
 
 type MainLayoutProps = {
   children: React.ReactNode;
@@ -62,19 +21,8 @@ type MainLayoutProps = {
 export const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div css={wrapperStyle}>
-      <header css={commonElementStyle}>
-        <div
-          css={css`
-            display: flex;
-            justify-content: space-between;
-          `}
-        >
-          <div>header</div>
-          <Navigation />
-        </div>
-      </header>
-      <main css={mainContainerStyle}>{children}</main>
-      <footer css={commonElementStyle}>
+      <main>{children}</main>
+      <footer css={footerStyle}>
         <div>footer</div>
       </footer>
     </div>
