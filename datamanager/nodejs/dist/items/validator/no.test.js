ea024fe778ade0f098a5eb7afdaca8545d097058
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-10-06 09:18:58 +0900

    add csv

--- datamanager/nodejs/dist/items/validator/no.test.js
+++ datamanager/nodejs/dist/items/validator/no.test.js
@@ -1,21 +1,17 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const no_1 = require('./no');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const no_1 = require("./no");
 describe('items/validator/url', () => {
-  const noValidator = new no_1.ItemValidatorNo();
-  test('文字列かどうか', () => {
-    expect(() => noValidator.validateDataType(1000)).toThrow('NOコードは文字列である必要があります。');
-    expect(() => noValidator.validateDataType(true)).toThrow('NOコードは文字列である必要があります。');
-  });
-  test('10桁かどうか', () => {
-    expect(() => noValidator.validateDataType('000302133322')).toThrow(
-      'NOコードは10桁である必要があります。10桁未満の場合、10桁になるように先頭を0で埋めてください。',
-    );
-    expect(() => noValidator.validateDataType('')).toThrow(
-      'NOコードは10桁である必要があります。10桁未満の場合、10桁になるように先頭を0で埋めてください。',
-    );
-  });
-  test('数値かどうか', () => {
-    expect(() => noValidator.validateDataType('0xdddf3a75')).toThrow('NOコードは数値である必要があります。');
-  });
+    const noValidator = new no_1.ItemValidatorNo();
+    test('文字列かどうか', () => {
+        expect(() => noValidator.validateDataType(1000)).toThrow('NOコードは文字列である必要があります。');
+        expect(() => noValidator.validateDataType(true)).toThrow('NOコードは文字列である必要があります。');
+    });
+    test('10桁かどうか', () => {
+        expect(() => noValidator.validateDataType('000302133322')).toThrow('NOコードは10桁である必要があります。10桁未満の場合、10桁になるように先頭を0で埋めてください。');
+        expect(() => noValidator.validateDataType('')).toThrow('NOコードは10桁である必要があります。10桁未満の場合、10桁になるように先頭を0で埋めてください。');
+    });
+    test('数値かどうか', () => {
+        expect(() => noValidator.validateDataType('0xdddf3a75')).toThrow('NOコードは数値である必要があります。');
+    });
 });
--- datamanager/nodejs/dist/items/validator/poi.test.js
+++ datamanager/nodejs/dist/items/validator/poi.test.js
@@ -1,21 +1,21 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const poi_1 = require('./poi');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const poi_1 = require("./poi");
 describe('items/validator/poi', () => {
-  const poiValidator = new poi_1.ItemValidatorPoi();
-  test('文字列かどうか', () => {
-    expect(() => poiValidator.validateDataType(1000)).toThrow('POIコードは文字列である必要があります。');
-    expect(() => poiValidator.validateDataType(true)).toThrow('POIコードは文字列である必要があります。');
-  });
-  test('4桁かどうか', () => {
-    expect(() => poiValidator.validateDataType('01010')).toThrow('POIコードは4桁である必要があります。');
-    expect(() => poiValidator.validateDataType('')).toThrow('POIコードは4桁である必要があります。');
-  });
-  test('数値かどうか', () => {
-    expect(() => poiValidator.validateDataType('abcd')).toThrow('POIコードは数値である必要があります。');
-  });
-  test('正しいPOIコードかどうか', () => {
-    expect(() => poiValidator.validateDataType('0101')).not.toThrow('存在しないPOIコードです。');
-    expect(() => poiValidator.validateDataType('1000')).toThrow('存在しないPOIコードです。');
-  });
+    const poiValidator = new poi_1.ItemValidatorPoi();
+    test('文字列かどうか', () => {
+        expect(() => poiValidator.validateDataType(1000)).toThrow('POIコードは文字列である必要があります。');
+        expect(() => poiValidator.validateDataType(true)).toThrow('POIコードは文字列である必要があります。');
+    });
+    test('4桁かどうか', () => {
+        expect(() => poiValidator.validateDataType('01010')).toThrow('POIコードは4桁である必要があります。');
+        expect(() => poiValidator.validateDataType('')).toThrow('POIコードは4桁である必要があります。');
+    });
+    test('数値かどうか', () => {
+        expect(() => poiValidator.validateDataType('abcd')).toThrow('POIコードは数値である必要があります。');
+    });
+    test('正しいPOIコードかどうか', () => {
+        expect(() => poiValidator.validateDataType('0101')).not.toThrow('存在しないPOIコードです。');
+        expect(() => poiValidator.validateDataType('1000')).toThrow('存在しないPOIコードです。');
+    });
 });
