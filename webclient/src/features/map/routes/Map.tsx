fc0f4b7644e8d26a6dd653ff2429a0ca8b6a84c0
Junya Ishihara <junya@champierre.com>
2022-10-15 10:59:45 +0900

    datasetが空のまま、ページがレンダリングされてしまう問題を修正

--- webclient/src/features/map/routes/Map.tsx
+++ webclient/src/features/map/routes/Map.tsx
@@ -9,32 +9,27 @@ import { useParams } from 'react-router-dom';
 export const Map: FC = () => {
   const { dataset_uid } = useParams<{ dataset_uid: string }>();
   const dataset = useGetDataset({ datasetUid: dataset_uid || '' });
-  console.log(dataset);
-
-  const telLink = (tel: string) => {
-    return `<a href="tel:${tel}">${tel}</a>`;
-  }
 
   const description = (point: any) => {
     return `<div>
       <strong>${point.名称}(${point.名称_カナ})</strong>
       <ul>
         <li>住所: ${point.住所}</li>
-        <li>電話番号: ${telLink(point.電話番号)}</li>
+        <li>電話番号: ${point.電話番号}</li>
       </ul>
     </div>`;
   }
 
-  const points = dataset.map(point => {
-    return {
-      title: point.名称,
-      description: description(point),
-      lat: Number(point.緯度),
-      lng: Number(point.経度),
-    }
-  });
-
-  console.log(points)
+  const points = dataset
+    .filter(point => point.緯度 && point.経度)
+    .map(point => {
+      return {
+        title: point.名称,
+        description: description(point),
+        lat: Number(point.緯度),
+        lng: Number(point.経度),
+      }
+    });
 
   const onLoad = useCallback((map: any) => {
     map.on('load', () => {
@@ -44,7 +39,7 @@ export const Map: FC = () => {
       const simpleStyle = new (window as any).geolonia.simpleStyle(geojson);
       simpleStyle.addTo(map).fitBounds();
     });
-  }, []);
+  }, [points]);
 
   return (
     <ContentLayout title="マップ">
