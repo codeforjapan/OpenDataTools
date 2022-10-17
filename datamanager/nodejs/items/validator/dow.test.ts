b53d856675d2ad184cc0cf2f3440ebfa79de4598
Junya Ishihara <junya@champierre.com>
2022-10-18 08:15:27 +0900

    曜日の漢字以外の文字が使われている場合と順番が正しくない場合とでエラーを分ける。

--- datamanager/nodejs/items/validator/dow.test.ts
+++ datamanager/nodejs/items/validator/dow.test.ts
@@ -9,29 +9,34 @@ describe('items/validator/dow', () => {
   });
 
   test('利用可能曜日が正しいかどうか', () => {
-    expect(() => dowValidator.validateDataType('日')).not.toThrow(
-      '利用可能曜日が正しくありません。曜日の漢字を並べて指定してください（例：火水木金土日）。',
-    );
-    expect(() => dowValidator.validateDataType('月水金')).not.toThrow(
-      '利用可能曜日が正しくありません。曜日の漢字を並べて指定してください（例：火水木金土日）。',
-    );
     expect(() => dowValidator.validateDataType('1000')).toThrow(
-      '利用可能曜日が正しくありません。曜日の漢字を並べて指定してください（例：火水木金土日）。',
+      '利用可能曜日が正しくありません。曜日の漢字以外の文字が使われています（例：月火水木金土日）。',
     );
     expect(() => dowValidator.validateDataType('月・水・金')).toThrow(
-      '利用可能曜日が正しくありません。曜日の漢字を並べて指定してください（例：火水木金土日）。',
+      '利用可能曜日が正しくありません。曜日の漢字以外の文字が使われています（例：月火水木金土日）。',
     );
     expect(() => dowValidator.validateDataType('月 水 金')).toThrow(
-      '利用可能曜日が正しくありません。曜日の漢字を並べて指定してください（例：火水木金土日）。',
+      '利用可能曜日が正しくありません。曜日の漢字以外の文字が使われています（例：月火水木金土日）。',
     );
-    expect(() => dowValidator.validateDataType('目月火')).toThrow(
-      '利用可能曜日が正しくありません。曜日の漢字を並べて指定してください（例：火水木金土日）。',
+    expect(() => dowValidator.validateDataType('月水目')).toThrow(
+      '利用可能曜日が正しくありません。曜日の漢字以外の文字が使われています（例：月火水木金土日）。',
     );
     expect(() => dowValidator.validateDataType('日月火')).toThrow(
-      '利用可能曜日が正しくありません。曜日の漢字を並べて指定してください（例：火水木金土日）。',
+      '利用可能曜日が正しくありません。曜日の漢字を正しく並べてください（例：月火水木金土日）。',
     );
     expect(() => dowValidator.validateDataType('水月金')).toThrow(
-      '利用可能曜日が正しくありません。曜日の漢字を並べて指定してください（例：火水木金土日）。',
+      '利用可能曜日が正しくありません。曜日の漢字を正しく並べてください（例：月火水木金土日）。',
     );
+
+    const wrapper = (arg: string) => {
+      try {
+        dowValidator.validateDataType(arg);
+      } catch {
+        throw new Error('An error has been occurred');
+      }
+      return true;
+    };
+    expect(wrapper('日')).toBe(true);
+    expect(wrapper('月水金')).toBe(true);
   });
 });
--- datamanager/nodejs/items/validator/dow.ts
+++ datamanager/nodejs/items/validator/dow.ts
@@ -3,9 +3,13 @@ export class ItemValidatorDow {
     if (typeof data !== 'string') {
       throw new Error('利用可能曜日は文字列である必要があります。');
     }
-    const matched = data.match(/^月?火?水?木?金?土?日?$/);
+    const matched = data.match(/^[月火水木金土日]+$/);
     if (!matched) {
-      throw new Error('利用可能曜日が正しくありません。曜日の漢字を並べて指定してください（例：火水木金土日）。');
+      throw new Error('利用可能曜日が正しくありません。曜日の漢字以外の文字が使われています（例：月火水木金土日）。');
+    }
+    const orderMatched = data.match(/^月?火?水?木?金?土?日?$/);
+    if (!orderMatched) {
+      throw new Error('利用可能曜日が正しくありません。曜日の漢字を正しく並べてください（例：月火水木金土日）。');
     }
     return;
   }
