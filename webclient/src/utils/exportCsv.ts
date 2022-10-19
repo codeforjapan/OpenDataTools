7b133f576386d510cf16ccc567accef71cf6c483
Kazuki Imamura <kaizumaki@gmail.com>
2022-10-19 09:55:00 +0900

    exportするcsvをbom付きにする

--- webclient/src/utils/exportCsv.ts
+++ webclient/src/utils/exportCsv.ts
@@ -4,9 +4,10 @@ export const exportCsv = (json: any[] | UnparseObject<any>) => {
   const csv = unparse(json, {
     header: true,
   });
-  const blob = new Blob([csv], { type: 'text/csv' });
+  const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
+  const blob = new Blob([bom, csv], { type: 'text/csv;charset=utf-8' });
   const link = document.createElement('a');
   link.href = window.URL.createObjectURL(blob);
-  link.download = 'Result.csv';
+  link.download = 'temp.csv'; // TODO: ダウンロードcsvのファイル名を可変にする
   link.click();
 };
