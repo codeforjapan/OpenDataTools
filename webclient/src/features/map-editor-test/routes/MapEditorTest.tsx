048cbfb11d1e55b4bb6e82e0047e86a87a922f6d
kur00 <over.y31.bsk@gmail.com>
2022-10-24 13:02:58 +0900

    feat: created map editor test page

---
+++ webclient/src/features/map-editor-test/routes/MapEditorTest.tsx
@@ -0,0 +1,29 @@
+import { Box, Button, useDisclosure } from '@chakra-ui/react';
+import { FC, useState } from 'react';
+import { ContentLayout } from '../../../components/Layout';
+import { MapEditorModal } from '../../../components/Editor';
+
+export const MapEditorTest: FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [lat, setLat] = useState<string | undefined>(undefined);
+  const [lng, setLng] = useState<string | undefined>(undefined);
+
+  return (
+    <ContentLayout title="マップ">
+      <Box>
+        <Button onClick={onOpen}>Open Modal</Button>
+      </Box>
+      <MapEditorModal
+        isOpen={isOpen}
+        onClose={onClose}
+        initialLat={lat}
+        initialLng={lng}
+        onComplete={(lat, lng) => {
+          setLat(lat);
+          setLng(lng);
+          onClose;
+        }}
+      />
+    </ContentLayout>
+  );
+};
