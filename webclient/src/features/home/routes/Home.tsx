153006a6af52cde5b3be47a25d2165f6c10d454a
Kazuki Imamura <kaizumaki@gmail.com>
2022-10-31 13:08:36 +0900

    homeに「CSVを元にMapを表示する」リンクボタンを仮置き

--- webclient/src/features/home/routes/Home.tsx
+++ webclient/src/features/home/routes/Home.tsx
@@ -1,11 +1,20 @@
-import { Box } from '@chakra-ui/react';
+import { Avatar, Box, Flex } from '@chakra-ui/react';
 import { ContentLayout } from '../../../components/Layout';
-import { OstLink } from '../../../components/Elements/OstLink';
+import { OstLink, OstNavLink } from '../../../components/Elements/OstLink';
 import { HomeSavedDatasetList } from '../../../components/Home';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
 export const Home = () => {
   return (
     <ContentLayout title="ホーム">
       <Box>ホーム</Box>
+      <Flex mt={4} justifyContent="flex-end">
+        <OstNavLink
+          to="/upload-file-for-map"
+          iconRight={<Avatar size="md" p="12px" icon={<ArrowForwardIcon />} />}
+        >
+          CSVを元にMapを表示する
+        </OstNavLink>
+      </Flex>
       <HomeSavedDatasetList />
       <Box w="300px">
         <OstLink to="/upload-file">はじめる（CSV選択画面）</OstLink>
