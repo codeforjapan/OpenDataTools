e41fc55f4082c48be25db8b28de8bbf8ac07194b
kur00 <over.y31.bsk@gmail.com>
2022-11-06 20:40:12 +0900

    fix: add handle selecting item logic on nav bar

--- webclient/src/components/Editor/DataEditorSidenav.tsx
+++ webclient/src/components/Editor/DataEditorSidenav.tsx
@@ -1,6 +1,6 @@
 import { Flex, Box, Text } from '@chakra-ui/react';
 import { InfoOutlineIcon, ChevronRightIcon } from '@chakra-ui/icons';
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router';
 import { useRecoilValue } from 'recoil';
 import { datasetItemAtom, datasetItemListSelector } from '../../stores/dataset';
@@ -16,6 +16,7 @@ export const DataEditorSidenav: FC<Props> = ({ onSelect, selectedItemUid }) => {
     datasetItemListSelector({ datasetUid: String(dataset_uid) })
   );
   const activeDatasetItemList = datasetItemList.filter((item) => item.isActive);
+  const LatLngUids = datasetItemList.filter((item) => ["緯度", "経度"].includes(item.normalizedLabel || "")).map((item) => item.uid);
   const [selectedItemUidState, setSelectedItemUidState] = useState<string>('');
 
   useEffect(() => {
@@ -34,13 +35,19 @@ export const DataEditorSidenav: FC<Props> = ({ onSelect, selectedItemUid }) => {
       onSelect(item.uid);
     };
 
+    const isSelectedItem = useMemo(() => {
+      // note: 緯度と経度の時だけ両方選択された状態にする
+      if(LatLngUids.includes(selectedItemUidState) && LatLngUids.includes(item?.uid || "")) return true
+      return item?.uid === selectedItemUidState
+    },[LatLngUids, selectedItemUidState])
+
     return (
       <Flex
         alignItems="center"
         justifyContent="space-between"
         p={4}
         sx={
-          item?.uid === selectedItemUidState
+          isSelectedItem
             ? {
                 bg: 'information.bg.disabled',
                 borderLeft: '3px solid',
@@ -60,7 +67,7 @@ export const DataEditorSidenav: FC<Props> = ({ onSelect, selectedItemUid }) => {
             {item?.normalizedLabel || item?.rowLabel}
           </Text>
         </Flex>
-        {item?.uid === selectedItemUidState && <ChevronRightIcon />}
+        {isSelectedItem && <ChevronRightIcon />}
       </Flex>
     );
   };
