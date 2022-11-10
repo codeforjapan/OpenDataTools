9d4cbe17270ff6450a91819b0bfe7d6e6a6a62c8
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-11-10 19:11:42 +0900

    add 2bytes2byte

--- datamanager/nodejs/items/validator/address.ts
+++ datamanager/nodejs/items/validator/address.ts
@@ -9,14 +9,20 @@ export class ItemValidatorAddress {
     const result = await normalize(data);
 
     switch (result.level) {
-    case 0:
-      throw new Error('住所が正しくありません。都道府県、市区町村と町丁目も入力してください（例：北→北海道札幌市西区24-2-2-3-3）。');
-    case 1:
-      throw new Error('住所が正しくありません。市区町村と町丁目も入力してください（例：北海道→北海道札幌市西区24-2-2-3-3）。');
-    case 2:
-      throw new Error('住所が正しくありません。町丁目も入力してください（例：北海道札幌市西区→北海道札幌市西区24-2-2-3-3）。');
-    default:
-      break;
+      case 0:
+        throw new Error(
+          '住所が正しくありません。都道府県、市区町村と町丁目も入力してください（例：北→北海道札幌市西区24-2-2-3-3）。',
+        );
+      case 1:
+        throw new Error(
+          '住所が正しくありません。市区町村と町丁目も入力してください（例：北海道→北海道札幌市西区24-2-2-3-3）。',
+        );
+      case 2:
+        throw new Error(
+          '住所が正しくありません。町丁目も入力してください（例：北海道札幌市西区→北海道札幌市西区24-2-2-3-3）。',
+        );
+      default:
+        break;
     }
 
     return;
