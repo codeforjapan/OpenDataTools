913fa2e8db6172f42239c1f54ccfdb8f00b72f87
kota-yata <kota@yatagai.com>
2022-12-11 23:24:46 +0900

    Apply original Moji to 2bytes2byte

--- datamanager/nodejs/utils/multibyte.test.ts
+++ datamanager/nodejs/utils/multibyte.test.ts
@@ -2,27 +2,27 @@ import { Moji } from './multibyte';
 
 describe('utils/multibyte', () => {
   test('全角英数字から半角英数字', () => {
-    const test1 = new Moji('１０９０１２３').convert('alphanumeric', 'ZtoH');
+    const test1 = new Moji('１０９０１２３').convert('alphanumeric', 'ZtoH').toString();
     expect(test1).toEqual('1090123');
-    const test2 = new Moji('ＡＺmo204４3').convert('alphanumeric', 'ZtoH');
+    const test2 = new Moji('ＡＺmo204４3').convert('alphanumeric', 'ZtoH').toString();
     expect(test2).toEqual('AZmo20443');
   });
   test('半角英数字から全角英数字', () => {
-    const test1 = new Moji('1090123').convert('alphanumeric', 'HtoZ');
+    const test1 = new Moji('1090123').convert('alphanumeric', 'HtoZ').toString();
     expect(test1).toEqual('１０９０１２３');
-    const test2 = new Moji('Azmo20443').convert('alphanumeric', 'HtoZ');
+    const test2 = new Moji('Azmo20443').convert('alphanumeric', 'HtoZ').toString();
     expect(test2).toEqual('Ａｚｍｏ２０４４３');
   });
   test('半角カナから全角カナ', () => {
-    const test1 = new Moji('｢ﾓﾄｸﾛｽﾊﾞｲｸ｣').convert('kana', 'HtoZ');
+    const test1 = new Moji('｢ﾓﾄｸﾛｽﾊﾞｲｸ｣').convert('kana', 'HtoZ').toString();
     expect(test1).toEqual('「モトクロスバイク」');
-    const test2 = new Moji('ﾃﾞｨｽﾞﾆ-ランﾄﾞ').convert('kana', 'HtoZ');
-    expect(test2).toEqual('ディズニーランド');
+    const test2 = new Moji('ﾃﾞｨｽﾞﾆ-ランﾄﾞ').convert('kana', 'HtoZ').toString();
+    expect(test2).toEqual('ディズニ－ランド');
   });
   test('全角カナから半角カナ', () => {
-    const test1 = new Moji('「モトクロスバイク」').convert('kana', 'ZtoH');
+    const test1 = new Moji('「モトクロスバイク」').convert('kana', 'ZtoH').toString();
     expect(test1).toEqual('｢ﾓﾄｸﾛｽﾊﾞｲｸ｣');
-    const test2 = new Moji('ディズニーﾗﾝド').convert('kana', 'ZtoH');
+    const test2 = new Moji('ディズニ－ﾗﾝド').convert('kana', 'ZtoH').toString();
     expect(test2).toEqual('ﾃﾞｨｽﾞﾆ-ﾗﾝﾄﾞ');
   });
   test('全角文字検知', () => {
--- datamanager/nodejs/utils/multibyte.ts
+++ datamanager/nodejs/utils/multibyte.ts
@@ -7,7 +7,7 @@ type MojiByte = 'ascii' | 'non-ascii';
 export class Moji {
   private H_KANA = ['ｳﾞ', 'ｶﾞ', 'ｷﾞ', 'ｸﾞ', 'ｹﾞ', 'ｺﾞ', 'ｻﾞ', 'ｼﾞ', 'ｽﾞ', 'ｾﾞ', 'ｿﾞ', 'ﾀﾞ', 'ﾁﾞ', 'ﾂﾞ', 'ﾃﾞ', 'ﾄﾞ', 'ﾊﾞ', 'ﾋﾞ', 'ﾌﾞ', 'ﾍﾞ', 'ﾎﾞ', 'ﾊﾟ', 'ﾋﾟ', 'ﾌﾟ', 'ﾍﾟ', 'ﾎﾟ', 'ｱ', 'ｲ', 'ｳ', 'ｴ', 'ｵ', 'ｶ', 'ｷ', 'ｸ', 'ｹ', 'ｺ', 'ｻ', 'ｼ', 'ｽ', 'ｾ', 'ｿ', 'ﾀ', 'ﾁ', 'ﾂ', 'ﾃ', 'ﾄ', 'ﾅ', 'ﾆ', 'ﾇ', 'ﾈ', 'ﾉ', 'ﾊ', 'ﾋ', 'ﾌ', 'ﾍ', 'ﾎ', 'ﾏ', 'ﾐ', 'ﾑ', 'ﾒ', 'ﾓ', 'ﾔ', 'ﾕ', 'ﾖ', 'ﾗ', 'ﾘ', 'ﾙ', 'ﾚ', 'ﾛ', 'ﾜ', 'ｦ', 'ﾝ', 'ｧ', 'ｨ', 'ｩ', 'ｪ', 'ｫ', 'ｬ', 'ｭ', 'ｮ', 'ｯ', '､', '｡', '｢', '｣', '-'];
   private hKanaRegex = new RegExp('(' + this.H_KANA.join('|') + ')', 'g');
-  private Z_KANA = ['ヴ', 'ガ', 'ギ', 'グ', 'ゲ', 'ゴ', 'ザ', 'ジ', 'ズ', 'ゼ', 'ゾ', 'ダ', 'ヂ', 'ヅ', 'デ', 'ド', 'バ', 'ビ', 'ブ', 'ベ', 'ボ', 'パ', 'ピ', 'プ', 'ペ', 'ポ', 'ア', 'イ', 'ウ', 'エ', 'オ', 'カ', 'キ', 'ク', 'ケ', 'コ', 'サ', 'シ', 'ス', 'セ', 'ソ', 'タ', 'チ', 'ツ', 'テ', 'ト', 'ナ', 'ニ', 'ヌ', 'ネ', 'ノ', 'ハ', 'ヒ', 'フ', 'ヘ', 'ホ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', 'ユ', 'ヨ', 'ラ', 'リ', 'ル', 'レ', 'ロ', 'ワ', 'ヲ', 'ン', 'ァ', 'ィ', 'ゥ', 'ェ', 'ォ', 'ャ', 'ュ', 'ョ', 'ッ', '、', '。', '「', '」', 'ー'];
+  private Z_KANA = ['ヴ', 'ガ', 'ギ', 'グ', 'ゲ', 'ゴ', 'ザ', 'ジ', 'ズ', 'ゼ', 'ゾ', 'ダ', 'ヂ', 'ヅ', 'デ', 'ド', 'バ', 'ビ', 'ブ', 'ベ', 'ボ', 'パ', 'ピ', 'プ', 'ペ', 'ポ', 'ア', 'イ', 'ウ', 'エ', 'オ', 'カ', 'キ', 'ク', 'ケ', 'コ', 'サ', 'シ', 'ス', 'セ', 'ソ', 'タ', 'チ', 'ツ', 'テ', 'ト', 'ナ', 'ニ', 'ヌ', 'ネ', 'ノ', 'ハ', 'ヒ', 'フ', 'ヘ', 'ホ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', 'ユ', 'ヨ', 'ラ', 'リ', 'ル', 'レ', 'ロ', 'ワ', 'ヲ', 'ン', 'ァ', 'ィ', 'ゥ', 'ェ', 'ォ', 'ャ', 'ュ', 'ョ', 'ッ', '、', '。', '「', '」', '－'];
   private zKanaRegex = new RegExp('(' + this.Z_KANA.join('|') + ')', 'g');
   private MULTIBYTE_EXP = /[^\x01-\x7E]+/;
   private SINGLE_BYTE_EXP = /[\x01-\x7E]+/;
@@ -15,19 +15,26 @@ export class Moji {
   constructor(str: string) {
     this.targetString = str;
   }
-  public convert(category: MojiCategory, direction: MojiDirection): string {
+  public convert(category: MojiCategory, direction: MojiDirection): Moji {
     if (category === 'alphanumeric') {
       const addition = direction === 'ZtoH' ? -65248 : 65248;
-      const regex = direction === 'ZtoH' ? /[Ａ-Ｚａ-ｚ０-９]/g : /[A-Za-z0-9]/g;
-      return this.targetString.replace(regex, (char: string) => String.fromCharCode(char.charCodeAt(0) + addition));
+      const regex = direction === 'ZtoH' ? /[！-～]/g : /[A-Za-z0-9]/g;
+      this.targetString = this.targetString.replace(regex, (char: string) =>
+        String.fromCharCode(char.charCodeAt(0) + addition)
+      );
+      return this;
     }
     const targetTable = direction === 'ZtoH' ? this.Z_KANA : this.H_KANA;
     const conversionTable = direction === 'ZtoH' ? this.H_KANA : this.Z_KANA;
     const regex = direction === 'ZtoH' ? this.zKanaRegex : this.hKanaRegex;
-    return this.targetString.replace(regex, (char: string) => {
+    this.targetString = this.targetString.replace(regex, (char: string) => {
       const index = targetTable.indexOf(char);
       return conversionTable[index];
     });
+    return this;
+  }
+  public toString(): string {
+    return this.targetString;
   }
   public doesContain(sd: MojiByte) {
     const regex = sd === 'ascii' ? this.SINGLE_BYTE_EXP : this.MULTIBYTE_EXP;
